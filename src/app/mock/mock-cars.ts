b38e6685949da64915dbef984b769535c4057fc7
Roseffc <roseffc@gmail.com>
2022-09-21 20:31:06 -0300

    Criado estrutura de componentes iniciais, rota, mock e adicionado imagens.

---
+++ src/app/mock/mock-cars.ts
@@ -0,0 +1,257 @@
+export const carsMock = [
+  {
+      "id": "01",
+      "placa": "EMG2090",
+      "chassi": "003067895090",
+      "renavam": "603050908",
+      "modelo": "Civic 2.0 LX",
+      "marca":"Honda",
+      "ano":"2022",
+      "urlImg":"/assets/HondaCivic",
+      "detalhes":{
+          "motorizacao":2.0,
+          "cambio":"CVT",
+          "potência":155,
+          "direção":2.0
+      }
+  },
+
+  {
+      "id": "02",
+      "placa": "BRT2895",
+      "chassi": "115600105681",
+      "renavam": "125894789",
+      "modelo": "Mercedes-Benz GLB",
+      "marca":"Mercedes-Bens",
+      "ano":"2022",
+      "urlImg":"/assets/MercedesGLB",
+      "detalhes":{
+          "motorizacao":1.3,
+          "cambio":"dupla embreagem man.sequ.com modo auto de 7 marchas",
+          "potência":163,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "03",
+      "placa": "RVD2453",
+      "chassi": "015398532042",
+      "renavam": "854336980",
+      "modelo": "Fit 1.5 DX",
+      "marca":"Honda",
+      "ano":"2021",
+      "urlImg":"/assets/HondaFit",
+      "detalhes":{
+          "motorizacao":1.5,
+          "cambio":"manual de 5 marchas",
+          "potência":115,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "04",
+      "placa": "RRM9860",
+      "chassi": "005610580656",
+      "renavam": "002689356",
+      "modelo": "Fiat Mobi",
+      "marca":"Fiat",
+      "ano":"2022",
+      "urlImg":"/assets/fiatMobi.png",
+      "detalhes":{
+          "motorizacao":1.0,
+          "cambio":"CVT com modo manual",
+          "potência":110,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "05",
+      "placa": "EGM5680",
+      "chassi": "256385421074",
+      "renavam": "950053860",
+      "modelo": "Honda HR-V",
+      "marca":"Honda",
+      "ano":"2022",
+      "urlImg":"/assets/HondaHRV",
+      "detalhes":{
+          "motorizacao":1.5,
+          "cambio":"CVT com modo manual",
+          "potência":126,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "06",
+      "placa": "LNA9060",
+      "chassi": "228965005986",
+      "renavam": "226987550",
+      "modelo": "Volkswagen Jetta",
+      "marca":"Volkswagen",
+      "ano":"2020",
+      "urlImg":"/assets/VolkswagenJetta",
+      "detalhes":{
+          "motorizacao":2.0,
+          "cambio":"dupla embreagem man.sequ.com modo auto de 6 marchas",
+          "potência":230,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "07",
+      "placa": "BTA3012",
+      "chassi": "001863102347",
+      "renavam": "920110020",
+      "modelo": "Honda Accord",
+      "marca":"Honda",
+      "ano":"2021",
+      "urlImg":"/assets/HondaAccord",
+      "detalhes":{
+          "motorizacao":2.0,
+          "cambio":"CVT",
+          "potência":212,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "08",
+      "placa": "PNR2560",
+      "chassi": "226587001129",
+      "renavam": "226988700",
+      "modelo": "Renault Duster",
+      "marca":"Renault",
+      "ano":"2022",
+      "urlImg":"/assets/RenaultDuster",
+      "detalhes":{
+          "motorizacao":1.6,
+          "cambio":"manual de 5 marchas",
+          "potência":120,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "09",
+      "placa": "NBR7530",
+      "chassi": "002356106950",
+      "renavam": "560056036",
+      "modelo": "Honda City",
+      "marca":"Honda",
+      "ano":"2022",
+      "urlImg":"/assets/HondaAccord",
+      "detalhes":{
+          "motorizacao":1.5,
+          "cambio":"CVT",
+          "potência":126,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "10",
+      "placa": "PNR2690",
+      "chassi": "558966005589",
+      "renavam": "005598685",
+      "modelo": "Ford Ecosport",
+      "marca":"Ford",
+      "ano":"2020",
+      "urlImg":"/assets/FordEcoSport",
+      "detalhes":{
+          "motorizacao":1.5,
+          "cambio":"automática de 6 marchas",
+          "potência":137,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "11",
+      "placa": "MNA2050",
+      "chassi": "105356008060",
+      "renavam": "050928056",
+      "modelo": "Honda City Hatchback",
+      "marca":"Honda",
+      "ano":"2021",
+      "urlImg":"/assets/HondaCityHatchback",
+      "detalhes":{
+          "motorizacao":1.5,
+          "cambio":"CVT com modo manual",
+          "potência":126,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "12",
+      "placa": "LNT0020",
+      "chassi": "102986955569",
+      "renavam": "228795630",
+      "modelo": "Chevrolet Montana",
+      "marca":"Chevrolet",
+      "ano":"2020",
+      "urlImg":"/assets/ChevroletMontana",
+      "detalhes":{
+          "motorizacao":1.4,
+          "cambio":"manual de 5 marchas",
+          "potência":99,
+          "direção":"hidráulica"
+      }
+  },
+
+  {
+      "id": "13",
+      "placa": "LFA1590",
+      "chassi": "002500254121",
+      "renavam": "569887113",
+      "modelo": "Honda CR-V",
+      "marca":"Honda",
+      "ano":"2021",
+      "urlImg":"/assets/HondaCRV",
+      "detalhes":{
+          "motorizacao":1.5,
+          "cambio":"CVT com modo manual",
+          "potência":190,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "14",
+      "placa": "MAN2022",
+      "chassi": "192022259923",
+      "renavam": "891084212",
+      "modelo": "Hyundai HB20",
+      "marca":"Hyundai",
+      "ano":"2020",
+      "urlImg":"/assets/HyundaiHB20",
+      "detalhes":{
+          "motorizacao":1.0,
+          "cambio":"manual de 5 marchas",
+          "potência":80,
+          "direção":"eletrica"
+      }
+  },
+
+  {
+      "id": "15",
+      "placa": "MRM8075",
+      "chassi": "559600255598",
+      "renavam": "006988556",
+      "modelo": "Honda WR-V",
+      "marca":"Honda",
+      "ano":"2021",
+      "urlImg":"/assets/HondaWRV",
+      "detalhes":{
+          "motorizacao":1.5,
+          "cambio":"CVT",
+          "potência":116,
+          "direção":"eletrica"
+      }
+  }
+
+]
